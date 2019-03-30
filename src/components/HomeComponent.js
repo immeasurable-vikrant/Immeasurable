fe4657b5126facbd738c18fdf907f9f9f2468b9b
vikrant <vikrant.immeasurable@gmail.com>
2019-03-31 03:08:56 +0530

    Home Component Updated 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -6,40 +6,69 @@ const Home = (props) => {
   return (
     <div className="conatiner">
        <div className="row">
-          <div className="col-6 ">
+          <div className="col-6">
       <Card>
       <CardImg top width="100%" src="https://wallpapercave.com/wp/wp3137137.jpg"/>
       </Card>
           </div>
 
-       <div className="col-3">
+      <div className="col-6">
+      <div className="row">
+       <div className="col-6">
       <Card>
       <CardImg top width="100%" src="https://wallpapercave.com/wp/wp3137244.jpg"/>
       </Card>
           </div>
 
-          <div className="col-3">
+          <div className="col-6">
       <Card>
       <CardImg top width="100%" src="https://wallpapercave.com/wp/wc3137263.jpg"/>
       </Card>
           </div>
+          </div>
 
-          <div className="col-3">
+        <div className="row">
+          <div className="col-6">
       <Card>
-      <CardImg top width="100%" src="https://www.pixelstalk.net/wp-content/uploads/wallpapers/Messi-vs-El-Clasico-2017-Wallpaper-1-620x349.jpg"/>
+      <CardImg top width="100%" src="https://images5.alphacoders.com/521/521476.jpg"/>
       </Card>
           </div>
 
-          <div className="col-3">
+          <div className="col-6">
       <Card>
-      <CardImg top width="100%" src="https://www.pixelstalk.net/wp-content/uploads/wallpapers/Messi-vs-El-Clasico-2017-Wallpaper-4-620x465.jpg"/>
+      <CardImg top width="100%" src="https://e00-marca.uecdn.es/imagenes/2015/01/22/en/football/barcelona/1421932390_extras_noticia_foton_7_0.jpg"/>
       </Card>
           </div>
         </div>
+        </div>
+        </div>
+
+        <div className="row">
+          <div className="col-4">
+            <h4>Unlimited Access</h4>
+            <p>Choose what you'd like to learn from 
+            our extensive subscription Library.</p>
+            </div>
+      
+        <div className="col-4">
+            <h4>Expert Masters</h4>
+            <p>Learn from industry experts who are 
+              passionate about Teaching.
+            </p>
+          </div>
+
+          <div className="col-4">
+            <h4>Learn Anywhere</h4>
+            <p>Switch between your copmputer, tablet or mobile device.</p>
+          </div>
+          <Button color="warning" >Subscribe now</Button>
+          </div>
+       
+        
 
 
         <div className="row">
-        <div className="col-4">
+        <div className="col-6 col-md-6">
         
         <Card>
         <CardImg top width="100%" src="https://images7.alphacoders.com/770/thumb-350-770680.jpg" />
@@ -52,8 +81,8 @@ const Home = (props) => {
       </Card>
       </div>
         </div>
-
-        <div className="col-4">
+        <div className="row">
+        <div className="col-6 col-md-6">
         
         <Card>
         <CardImg top width="100%" src="https://images2.alphacoders.com/144/thumb-1920-144270.jpg" alt="Card image cap" />
@@ -64,7 +93,7 @@ const Home = (props) => {
           <Button color="warning">Preview</Button>
         </CardBody>
       </Card>
-      
+      </div>
         </div>
     </div>   
     
